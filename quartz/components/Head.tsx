06dc638be73aeab4b09defb6f4f02f218eb4c7d5
indirawrs <indira@nyu.edu>
2026-08-11 15:39:02 -0400

    Quartz sync: Aug 11, 2026, 3:39 PM

--- quartz/components/Head.tsx
+++ quartz/components/Head.tsx
@@ -106,6 +106,9 @@ export default (() => {
             return resource
           }
         })}
+<link rel="preconnect" href="https://fonts.googleapis.com" />
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+<link href="https://fonts.googleapis.com/css2?family=Cormorant+Garamond:ital,wght@0,300;0,400;1,300&family=Josefin+Sans:wght@200;300&display=swap" rel="stylesheet" />
       </head>
     )
   }
